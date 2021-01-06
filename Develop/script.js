15b261ff0c4b9605c1a07a64e4607eb38f73009b
Chris Gutierrez <cmgson1@gmail.com>
2021-01-05 23:29:08 -0600

    added variables,prompt for length and console check

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var special = "!@#$%^&*-_=+`~?\|";
+
+var pwlength = prompt("please pick a length between 8 and 128 characters");
+console.log (pwlength);
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +16,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
