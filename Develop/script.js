043f82bd47968c2c2c90c7cd7e4d9e4c360e6c36
Chris Gutierrez <cmgson1@gmail.com>
2021-01-06 01:25:20 -0600

    added the for loop to set the password as well as the final if statement to make sure they picked some set of characters

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,7 @@ var numbers = "0123456789";
 var special = "!@#$%^&*-_=+`~?\|";
 var charset = "";
 var pwlength = "";
+var password = "";
 
 
 //initialize pwlength then set while checking validity
@@ -22,7 +23,7 @@ var lowerConfirm = confirm("would you like to add lower case to your password?")
   }
   //check if it updated with console
   console.log (charset);
-
+//confirm uppercase
   var upperConfirm = confirm("would you like to add upper case to your password?")
   if (upperConfirm) {
     alert("Ok! I see you!");
@@ -30,7 +31,7 @@ var lowerConfirm = confirm("would you like to add lower case to your password?")
   }
 
   console.log (charset);
-
+//confirm numbers
   var numbersConfirm = confirm("how about a few numbers in there?")
   if (numbersConfirm) {
     alert("Dang! they aint ever gunna guess this one!");
@@ -38,6 +39,30 @@ var lowerConfirm = confirm("would you like to add lower case to your password?")
   }
 
   console.log (charset);
+
+  var specialConfirm = confirm("Dash of special characters?")
+  if (specialConfirm)  {
+    alert("Oh you fancy huh?");
+    charset += special;
+  }
+
+  console.log (charset);
+
+  if (charset === '') {
+    alert('might as well make your password "password"');
+    alert("lets try again shall we?  this time select at least one set of characters to use please");
+  } else {
+    alert('abracadabra!');
+  }
+
+  for (var i = 0; i < pwlength; i++) {
+    password += charset.charAt (Math.floor(Math.random()*charset.length))
+    
+  }
+
+  console.log(password);
+
+
 
 // Write password to the #password input
 function writePassword() {
